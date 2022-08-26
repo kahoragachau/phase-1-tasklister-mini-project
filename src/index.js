d01f60805ec5efa9879aee6124e929ce77d03939
kahoragachau <joelkahoragachau6@gmail.com>
2022-08-26 13:20:36 +0300

    - Create Add functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("submit", (e) => {
+    event.preventDefault()
+    const getText = document.getElementById("new-task-description")
+    const li = document.createElement("li")
+    li.append(getText.value)
+    document.getElementById("tasks").append(li)
+  })
 });
