7a6ff4b77b0c9c0c4b42b37feae192d6ea3e43dc
kahoragachau <joelkahoragachau6@gmail.com>
2022-08-26 13:28:15 +0300

    - create button for delete purposes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault()
     const getText = document.getElementById("new-task-description")
     const li = document.createElement("li")
-    li.append(getText.value)
+    const btn = document.createElement("button")
+    btn.innerText = "X"
+    btn.style.marginLeft = "50px"
+    btn.style.backgroundColor = "red"
+    btn.style.color = "white"
+    btn.style.border = "none"
+    li.append(getText.value, btn)
     document.getElementById("tasks").append(li)
   })
 });
